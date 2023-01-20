38fd0d2250fab60c89d668a17589878898984c71
Philip <44257880+philiphand@users.noreply.github.com>
2023-01-20 13:22:54 +0100

    Intro program form

--- data/authors/default.md
+++ data/authors/default.md
@@ -11,13 +11,13 @@ linkedin: https://www.linkedin.com/company/effektiv-altruisme-uib/
 
 Vi gir studenter i Bergen kunnskap, motivasjon, og mulighet til å utgjøre en størst mulig positiv forskjell i verden. Sammen med oss kan du lære om en rekke globale saksområder som preger dagens verden. Vi ønsker alle studenter velkommen til å delta i det engasjerende sosiale miljøet vårt!
 
-Effektiv Altruisme UiB ble grunnlagt i september 2021 av tre studenter ved Universitetet i Bergen. I dag er vi en solid gjeng med studenter og diverse samarbeidspartnere fra alle verdens hjørner.
+Effektiv Altruisme UiB ble grunnlagt i september 2021 av tre studenter ved Universitetet i Bergen. I dag er vi en solid gjeng med studenter og samarbeidspartnere fra alle verdens hjørner.
 
 Vi er en del av det norske EA-miljøet: https://effektivaltruisme.no/uib
 
 ## Hva er effektiv altruisme?
 
-Målet med effektiv altruisme er å finne de mest effektive måtene å hjelpe andre eller på annet vis gjøre verden et bedre sted. For å oppnå dette best mulig sikter mange i EA-miljøet på å følge en rasjonell tankegang og å gjøre evidensbaserte beslutninger.
+Målet med effektiv altruisme er å finne de mest effektive måtene å hjelpe andre eller på annet vis gjøre verden et bedre sted. For å oppnå dette best mulig sikter mange i EA-miljøet på å følge en rasjonell tankegang med fokus på bruk av evidens.
 
 Det globale EA-miljøet er kontinuerlig på jakt etter hvordan man kan få mest igjen for de ressursene som brukes på å gjøre godt i verden. Høres dette spennende ut? Da vil vi gjerne ha deg med på laget!
 
--- pages/intro.js
+++ pages/intro.js
@@ -16,11 +16,25 @@ export default function Intro() {
             Introkurs i effektiv altruisme
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Lær om grunnprinsippene bak effektiv altruisme og hvordan du kan bidra til å gjøre
-            verden et bedre sted
+            Et 6-ukers kurs med ukentlige diskusjonsmøter om globale utfordringer og effektiv
+            altruisme
           </p>
+          <p className="text-lg font-bold leading-7 text-black dark:text-white">
+            Neste kurs begynner onsdag 15. februar (søkndadsfrist 9. februar)
+          </p>
+          <form target="_blank" action="https://airtable.com/shrVahPsEXzgfoK08">
+            <button
+              className={`w-3/5 rounded-md bg-primary-500 py-4 px-4 font-medium text-white hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-primary-600 focus:ring-offset-2 dark:ring-offset-black dark:hover:bg-primary-400`}
+              type="submit"
+            >
+              Meld deg på her
+            </button>
+          </form>
         </div>
         <div className="container py-12">
+          <h2 className="pb-4 text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-5xl md:leading-14">
+            Hva lærer du?
+          </h2>
           <div className="-m-4 flex flex-wrap">
             {introData.map((d) => (
               <Card
@@ -32,6 +46,19 @@ export default function Intro() {
               />
             ))}
           </div>
+        </div>
+        <div className="container py-12">
+          <p className="pb-4 text-lg font-bold leading-7 text-black dark:text-white">
+            Neste kurs begynner onsdag 15. februar (søkndadsfrist 9. februar)
+          </p>
+          <form target="_blank" action="https://airtable.com/shrVahPsEXzgfoK08">
+            <button
+              className={`w-3/5 rounded-md bg-primary-500 py-4 px-4 font-medium text-white hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-primary-600 focus:ring-offset-2 dark:ring-offset-black dark:hover:bg-primary-400`}
+              type="submit"
+            >
+              Meld deg på her
+            </button>
+          </form>
         </div>
       </div>
     </>
