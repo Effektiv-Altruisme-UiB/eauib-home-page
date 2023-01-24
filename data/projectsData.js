df799b6e8b2e40d886175bc0f69132d533f4bf76
Philip <44257880+philiphand@users.noreply.github.com>
2023-01-24 16:30:57 +0100

    Updated activities

--- data/projectsData.js
+++ data/projectsData.js
@@ -6,10 +6,21 @@ const projectsData = [
     href: '/intro',
   },
   {
+    title: 'Turer til EAG-konferanser',
+    description: `Vi drar i blant til EA Global-konferanser som byr på en intensiv helg med foredrag, workshops og muligheter for nettverksbygging.`,
+    imgSrc: '/static/images/ea_global.jpg',
+    href: '/blog/eagx-oxford',
+  },
+  {
     title: 'Hytteturer',
     description: `Bli med på hyttetur for å bli bedre kjent med gjengen vår, gå fjellturer, nyte hjemmelagde måltider og delta på programmer som ruster deg til å gjøre godt i verden.`,
     imgSrc: '/static/images/cabin.jpg',
     href: '/blog/hyttetur-2022',
+  },
+  {
+    title: 'Diverse sosialt',
+    description: `Vi møtes jevnlig for diverse sosialt som middager, fjellturer, quiz og brettspill! Vi har et aktivt sosialt miljø av både lokale studenter, utvekslingsstudenter og folk i arbeid!`,
+    imgSrc: '/static/images/park_social.jpg',
   },
 ]
 
